252e385965b62c54cb28ca5eaa84c849f7d542cc
Paras <118166649+Parasbarsaiya@users.noreply.github.com>
2022-12-23 16:57:08 +0530

    Update Table2.sql

--- Demo/Table2.sql
+++ Demo/Table2.sql
@@ -3,6 +3,9 @@ CREATE TABLE [dbo].[Table4] (
     [Paras ] CHAR (100) NULL,
     [demo]   NCHAR (10) NULL,
     [demo1]   NCHAR (10) NULL,
-    [demo2]   NCHAR (10) NULL
+    [demo2]   NCHAR (10) NULL,
+    [demo3]   NCHAR (10) NULL,
+    [demo4]   NCHAR (10) NULL,
+    [demo5]   NCHAR (10) NULL
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
